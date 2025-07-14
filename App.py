dde11865d304b605cdc67f0f441310011f7946d1
Tariq-qazi <tareq.qazizadeh@gmail.com>
2025-07-15 01:49:09 +0400

    Update App.py

--- App.py
+++ App.py
@@ -18,18 +18,17 @@ st.sidebar.title("Serdal Map Filters")
 unit_type = st.sidebar.selectbox("Select Unit Type", sorted(smart_groups['type'].unique()))
 rooms = st.sidebar.selectbox("Select Room Count", sorted(smart_groups['rooms'].unique()))
 quarter = st.sidebar.selectbox("Select Quarter", sorted(smart_groups['quarter'].unique(), reverse=True))
+insight_mode = st.sidebar.radio("View Insights For:", ["Investor", "End User"])
 
 # --- Filter Smart Groups ---
-filtered = smart_groups[
-    (smart_groups['type'] == unit_type) &
-    (smart_groups['rooms'] == rooms) &
-    (smart_groups['quarter'] == quarter)
-]
+filtered = smart_groups[(smart_groups['type'] == unit_type) &
+                        (smart_groups['rooms'] == rooms) &
+                        (smart_groups['quarter'] == quarter)]
 
 # --- Merge with GeoJSON using cleaned fields ---
 zones_filtered = zones.merge(filtered, left_on='CNAME_E_clean', right_on='area_clean')
 
-# --- Assign Color Based on Investor Recommendation ---
+# --- Assign Color Based on Recommendation ---
 def get_color(advice):
     if advice == 'Buy':
         return 'green'
@@ -38,8 +37,6 @@ def get_color(advice):
     elif advice == 'Avoid':
         return 'red'
     return 'gray'
-
-zones_filtered['color'] = zones_filtered['Recommendation_Investor'].apply(get_color)
 
 # --- Create Folium Map ---
 m = folium.Map(location=[25.2048, 55.2708], zoom_start=11)
@@ -54,31 +51,42 @@ for _, row in zones_filtered.iterrows():
             "Investor Insight": row['Insight_Investor'],
             "Investor Recommendation": row['Recommendation_Investor'],
             "End User Insight": row['Insight_EndUser'],
-            "End User Recommendation": row['Recommendation_EndUser'],
-            "color": row['color']
+            "End User Recommendation": row['Recommendation_EndUser']
         }
     }
 
+    if insight_mode == "Investor":
+        popup_html = f"""
+            <b>Area:</b> {feature['properties']['CNAME_E']}<br>
+            <b>Pattern ID:</b> {feature['properties']['Pattern ID']}<br>
+            <b><u>Insight:</u></b><br>{feature['properties']['Investor Insight']}<br>
+            <b><u>Recommendation:</u></b> {feature['properties']['Investor Recommendation']}<br>
+        """
+        color_source = feature['properties']['Investor Recommendation']
+    else:
+        popup_html = f"""
+            <b>Area:</b> {feature['properties']['CNAME_E']}<br>
+            <b>Pattern ID:</b> {feature['properties']['Pattern ID']}<br>
+            <b><u>Insight:</u></b><br>{feature['properties']['End User Insight']}<br>
+            <b><u>Recommendation:</u></b> {feature['properties']['End User Recommendation']}<br>
+        """
+        color_source = feature['properties']['End User Recommendation']
+
+    fill_color = get_color(color_source)
+
     folium.GeoJson(
         data=feature,
-        style_function=lambda x: {
-            'fillColor': x['properties']['color'],
+        style_function=lambda x, color=fill_color: {
+            'fillColor': color,
             'color': 'black',
             'weight': 1,
             'fillOpacity': 0.6
         },
         tooltip=folium.Tooltip(feature['properties']['CNAME_E']),
-        popup=folium.Popup(f"""
-            <b>Area:</b> {feature['properties']['CNAME_E']}<br>
-            <b>Pattern ID:</b> {feature['properties']['Pattern ID']}<br>
-            <b><u>Investor Insight:</u></b><br>{feature['properties']['Investor Insight']}<br>
-            <b><u>Investor Recommendation:</u></b> {feature['properties']['Investor Recommendation']}<br><br>
-            <b><u>End User Insight:</u></b><br>{feature['properties']['End User Insight']}<br>
-            <b><u>End User Recommendation:</u></b> {feature['properties']['End User Recommendation']}<br>
-        """, max_width=400)
+        popup=folium.Popup(popup_html, max_width=400)
     ).add_to(m)
 
 # --- Display Map ---
 st.title("Serdal SmartZone Map")
-st.markdown(f"**Quarter:** {quarter} | **Unit:** {unit_type} | **Rooms:** {rooms}")
+st.markdown(f"**Quarter:** {quarter} | **Unit:** {unit_type} | **Rooms:** {rooms} | **Mode:** {insight_mode}")
 st_folium(m, width=1200, height=700)
