7a85ac3c8d751ac00d33323e1a9f0635fb878882
Tariq-qazi <tareq.qazizadeh@gmail.com>
2025-07-15 01:28:27 +0400

    Update App.py

--- App.py
+++ App.py
@@ -1,4 +1,9 @@
-import streamlit as st import geopandas as gpd import pandas as pd import folium from streamlit_folium import st_folium
+import streamlit as st 
+import geopandas as gpd 
+import pandas as pd 
+import folium 
+from streamlit_folium import st_folium 
+from shapely.geometry import mapping
 
 --- Load Data ---
 
@@ -30,7 +35,26 @@ zones_filtered['color'] = zones_filtered['Recommendation_Investor'].apply(get_co
 
 m = folium.Map(location=[25.2048, 55.2708], zoom_start=11)
 
-for _, row in zones_filtered.iterrows(): folium.GeoJson( row['geometry'], style_function=lambda x, color=row['color']: { 'fillColor': color, 'color': 'black', 'weight': 1, 'fillOpacity': 0.6 }, tooltip=folium.Tooltip(row['CNAME_E']), popup=folium.Popup(f""" <b>Area:</b> {row['CNAME_E']}<br> <b>Pattern ID:</b> {row['pattern_id']}<br> <b><u>Investor Insight:</u></b><br>{row['Insight_Investor']}<br> <b><u>Investor Recommendation:</u></b> {row['Recommendation_Investor']}<br><br> <b><u>End User Insight:</u></b><br>{row['Insight_EndUser']}<br> <b><u>End User Recommendation:</u></b> {row['Recommendation_EndUser']}<br> """, max_width=400) ).add_to(m)
+for _, row in zones_filtered.iterrows(): feature = { "type": "Feature", "geometry": mapping(row['geometry']), "properties": { "CNAME_E": row['CNAME_E'], "Pattern ID": row['pattern_id'], "Investor Insight": row['Insight_Investor'], "Investor Recommendation": row['Recommendation_Investor'], "End User Insight": row['Insight_EndUser'], "End User Recommendation": row['Recommendation_EndUser'], "color": row['color'] } }
+
+folium.GeoJson(
+    data=feature,
+    style_function=lambda x: {
+        'fillColor': x['properties']['color'],
+        'color': 'black',
+        'weight': 1,
+        'fillOpacity': 0.6
+    },
+    tooltip=folium.Tooltip(feature['properties']['CNAME_E']),
+    popup=folium.Popup(f"""
+        <b>Area:</b> {feature['properties']['CNAME_E']}<br>
+        <b>Pattern ID:</b> {feature['properties']['Pattern ID']}<br>
+        <b><u>Investor Insight:</u></b><br>{feature['properties']['Investor Insight']}<br>
+        <b><u>Investor Recommendation:</u></b> {feature['properties']['Investor Recommendation']}<br><br>
+        <b><u>End User Insight:</u></b><br>{feature['properties']['End User Insight']}<br>
+        <b><u>End User Recommendation:</u></b> {feature['properties']['End User Recommendation']}<br>
+    """, max_width=400)
+).add_to(m)
 
 --- Display Map ---
 
